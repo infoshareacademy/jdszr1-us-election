c7d4654e902897184e49fc1323e6f156549645ab
Joanna Garwacka <j.garwacka@wp.pl>
2020-06-08 14:49:38 +0200

    create_table_candidates.sql and candidates.csv files added

---
+++ create_table_candidates.sql
@@ -0,0 +1,10 @@
+-- Utworzenie tabeli candidates, zawierającej dane o kandydatach
+  
+  create table candidates (
+  	candidate text,
+  	party text,
+  	gender text,
+  	rase text,
+  	year_of_birth int,
+  	age_2016 int
+  );
