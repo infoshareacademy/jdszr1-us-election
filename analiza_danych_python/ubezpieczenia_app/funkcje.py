a12d3817903a661f070983a2639a95f6dc32c12d
Joanna Garwacka <j.garwacka@wp.pl>
2020-08-15 16:37:46 +0200

    eng transtlation + user comments + option for no resuls added

--- analiza_danych_python/ubezpieczenia_app/funkcje.py
+++ analiza_danych_python/ubezpieczenia_app/funkcje.py
@@ -3,26 +3,26 @@ import os
 import numpy as np
 
 
-def pobierz_state():
+def choose_state():
     with open("dane/states.csv", "r") as plik:
         states = [linia.rstrip() for linia in plik]
-    print("Lista state codes:")
+    print("\nLista state codes:")
     print(states)
-    state_code = input("State code:")
+    state_code = input("\nPodaj kod stanu, w którym chcesz się ubezpieczyć:")
     if state_code in states:
         return state_code
     else:
-        print("Niepoprawny state code. Wybierz z listy.")
-        state_code = input("State code:")
+        print("\nNiepoprawny state code. Wybierz z listy.")
+        state_code = input("Podaj kod stanu, w którym chcesz się ubezpieczyć:")
         return state_code
 
 
-def pobierz_wiek():
+def choose_age():
     try:
-        age_input = int(input("Wiek:"))
+        age_input = int(input("\nPodaj swój wiek:"))
     except ValueError:
-        print("To nie jest liczba")
-        age_input = int(input("Wiek:"))
+        print("\nTo nie jest liczba")
+        age_input = int(input("Podaj swój wiek:"))
     
     if age_input <= 20:
         age = '0-20'
@@ -33,8 +33,8 @@ def pobierz_wiek():
     return str(age)
 
 
-def pobierz_opcje():
-    opcje_dict = {
+def choose_option():
+    opt_dict = {
         1:"IndividualRate",
         2:"Couple",
         3:"PrimarySubscriberAndOneDependent",
@@ -44,20 +44,29 @@ def pobierz_opcje():
         7:"CoupleAndTwoDependents",
         8:"CoupleAndThreeOrMoreDependents"
         }
-    print(" 2 - Couple\n 3 - Primary Subscriber and One Dependent\n 4 - Primary Subscriber and Two Dependents\n 5 - Primary Subscriber and Three or More Dependents\n 6 - Couple And One Dependent\n 7 - Couple And Two Dependents\n 8 - Couple And Three or More Dependents\n")
-    opcja = int(input("Podaj numer opcji ubezpieczenia:"))
-    if opcja in range(2,9):
-        return opcja
+    print("\nPoniższa lista prezentuje dostępne opcje ubezpieczenia rodzinnego:")
+    print("\n 2 - Couple\n 3 - Primary Subscriber and One Dependent\n 4 - Primary Subscriber and Two Dependents\n 5 - Primary Subscriber and Three or More Dependents\n 6 - Couple And One Dependent\n 7 - Couple And Two Dependents\n 8 - Couple And Three or More Dependents\n")
+    option = int(input("Z powyższej listy wybierz numer opcji ubezpieczenia:"))
+    if option in range(2,9):
+        return option
     else:
-        print("Nie ma takiej opcji. Wybierz numer z listy")
-        opcja = int(input("Podaj numer opcji ubezpieczenia:"))
-        return opcja
+        print("\nNie ma takiej opcji. Wybierz numer z listy")
+        option = int(input("Z powyższej listy wybierz numer opcji ubezpieczenia:"))
+        return option
 
+def get_data():
+    family = input("Czy chcesz się ubezpieczyć z rodziną? y/n:")
+    if family == 'y':
+        age = 'Family Option'
+        option = choose_option()
+    else:
+        age = choose_age()
+        option = 1
+    return (age, option)
 
+def show_result(state_code, age, option, network_df, pa_df, rate_df):
 
-def pokaz_wynik(state_code, age, opcja, network_df, pa_df, rate_df):
-
-    opcje_dict = {
+    opt_dict = {
         1:"IndividualRate",
         2:"Couple",
         3:"PrimarySubscriberAndOneDependent",
@@ -73,15 +82,15 @@ def pokaz_wynik(state_code, age, opcja, network_df, pa_df, rate_df):
     pa_df = pa_df.rename(columns = {'StandardComponentId':'PlanId'})
     join1 = pd.merge(rate_df, network_df, on="IssuerId", how="inner")
     join_final = pd.merge(join1, pa_df, on="PlanId", how="inner")
-    new_df = join_final[[opcje_dict[opcja], 'PlanId', 'PlanMarketingName', 'NetworkName']][(join_final["StateCode"] == state_code) & (join_final["Age"] == age) & (join_final[opcje_dict[opcja]] != 0)]
-    new_df = new_df.sort_values(opcje_dict[opcja])
-    q1 = np.percentile(new_df[opcje_dict[opcja]], 25)
-    q2 = np.percentile(new_df[opcje_dict[opcja]], 50)
-    q3 = np.percentile(new_df[opcje_dict[opcja]], 75)
-    min(new_df[opcje_dict[opcja]])
-    q0_df = new_df[new_df[opcje_dict[opcja]] == min(new_df[opcje_dict[opcja]])].head(n=1)
-    q1_df = new_df[new_df[opcje_dict[opcja]] == q1].head(n=1)
-    q2_df = new_df[new_df[opcje_dict[opcja]] == q2].head(n=1)
-    q3_df = new_df[new_df[opcje_dict[opcja]] == q3].head(n=1)
+    new_df = join_final[[opt_dict[option], 'PlanId', 'PlanMarketingName', 'NetworkName']][(join_final["StateCode"] == state_code) & (join_final["Age"] == age) & (join_final[opt_dict[option]] != 0)]
+    new_df = new_df.sort_values(opt_dict[option])
+    q1 = np.percentile(new_df[opt_dict[option]], 25)
+    q2 = np.percentile(new_df[opt_dict[option]], 50)
+    q3 = np.percentile(new_df[opt_dict[option]], 75)
+    min(new_df[opt_dict[option]])
+    q0_df = new_df[new_df[opt_dict[option]] == min(new_df[opt_dict[option]])].head(n=1)
+    q1_df = new_df[new_df[opt_dict[option]] == q1].head(n=1)
+    q2_df = new_df[new_df[opt_dict[option]] == q2].head(n=1)
+    q3_df = new_df[new_df[opt_dict[option]] == q3].head(n=1)
     final = pd.concat([q0_df,q1_df,q2_df,q3_df])
-    return final
+    return(final.reset_index(drop = True))
