e254b97fb5ee04e53d3be66a04c95063d9d017b6
monika <pmonis@wp.pl>
2020-10-10 09:42:37 +0200

    zmiany w  implementacji w jupiterze, zmiany w funkcjach, dodanie testow modeli

--- Machine_learning_project/insurance_app/functions.py
+++ Machine_learning_project/insurance_app/functions.py
@@ -3,7 +3,7 @@ from IPython.display import display
 import numpy as np
 import pandas as pd
 
-def widgets():
+def widgets_pro():
     import ipywidgets as widgets
     from IPython.display import display
     
@@ -90,11 +90,7 @@ def train_model(df, df_created):
     stawka = stawka.fillna(0)
     stawka['Stawka ubezpieczenia'] = charge.item()
     return stawka
+  
 
+    
 
-def click_button(button, sex_w, age_w, height_w, weight_w, smoker_w, children_w, region_w):
-    sex, age, bmi, smoker, children, region = values(sex_w, age_w, height_w, weight_w, smoker_w, children_w, region_w)
-    df_created = create_df(sex, age, bmi, smoker, children, region)
-    stawka = train_model(df, df_created)
-    print("Twoja stawka ubezpieczenia:")
-    print(stawka)
